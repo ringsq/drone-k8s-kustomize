e6e18f8c56f1b46010ba18e8f5bb15eb9cbaed8b
gaurav-teliax <gaurav.gaglani@teliax.com>
2021-07-27 17:56:47 +0530

    allow setting image path and version of init containers and update debug messages

--- plugin.sh
+++ plugin.sh
@@ -4,11 +4,14 @@ set -eu pipefail
 
 "${PLUGIN_DEBUG:-false}" && set -x && printenv
 
+echo ">>> Checking for kubeconfig for the k8s cluster config and k8s manifests directory path <<<"
+
 if [ -z "$PLUGIN_KUBECONFIG" ] || [ -z "$PLUGIN_FOLDERPATH" ]; then
     echo "KUBECONFIG and/or FOLDERPATH not supplied"
     exit 1
 fi
 
+echo ">>> Setting kubeconfig to access the k8s cluster <<<"
 if [ -n "$PLUGIN_KUBECONFIG" ];then
     [ -d $HOME/.kube ] || mkdir $HOME/.kube
     echo "# Plugin PLUGIN_KUBECONFIG available" >&2
@@ -16,41 +19,57 @@ if [ -n "$PLUGIN_KUBECONFIG" ];then
     unset PLUGIN_KUBECONFIG
 fi
 
-echo "---- Checking if migrations flag set ----"
+echo ">>> Checking for the deployment operation to be performed. It could be db migration job or k8s resource deployment like: deployment or namespace <<<"
 PLUGIN_MIGRATION_JOB="${PLUGIN_MIGRATION_JOB:-false}"
 if [ $PLUGIN_MIGRATION_JOB == true ]
     then
     PLUGIN_NAMESPACE="${PLUGIN_NAMESPACE:-default}"
     if [ PLUGIN_NAMESPACE != "default" ]
         then
-        echo "Deleting the k8s Job resource: ${PLUGIN_JOBNAME} in Namespace: ${PLUGIN_NAMESPACE}."
+        echo ">>> Deleting the k8s Job resource: ${PLUGIN_JOBNAME} in Namespace: ${PLUGIN_NAMESPACE}. <<<"
         kubectl delete -n ${PLUGIN_NAMESPACE} job/${PLUGIN_JOBNAME} || true
     else
-        echo "No namespace defined"
+        echo ">>> No namespace defined <<<"
     fi
 else
-    echo "Migration variable not detected. This is a regular deployment"
+    echo ">>> Migration variable not detected. This is a regular deployment <<<"
 fi
 
 cd "${PLUGIN_FOLDERPATH}"
 
 if [ $PLUGIN_MIGRATION_JOB == false ]
     then
-    echo "---- Not a migration job ----"
+    echo ">>> Executing k8s manifests at path provided $PLUGIN_FOLDERPATH.... <<<"
     DRONE_SEMVER="${tag:-$DRONE_SEMVER}"
     PLUGIN_IMAGE="${PLUGIN_IMAGE:-NULL}"
     if [ $PLUGIN_IMAGE == "NULL" ]; then
-        echo "Container Image not provided"
+        echo ">>> Don't need to set the image path and version for Containers <<<"
     else
-        echo "Setting the deployment image"
+        echo ">>> Setting the image path and version for Containers <<<"
         kustomize edit set image "$PLUGIN_IMAGE":$DRONE_SEMVER
     fi
+
+    PLUGIN_INIT_CONTAINERS="${PLUGIN_INIT_CONTAINERS:-NULL}"
+    if [ $PLUGIN_INIT_CONTAINERS == "NULL" ]; then
+        echo ">>> Don't need to set any initContainers' image paths and versions <<<"
+    else
+        echo ">>> Setting the initContainers' image paths and versions <<<"
+        original_ifs="$IFS"
+        IFS="|"
+        for image in $PLUGIN_INIT_CONTAINERS
+        do
+            kustomize edit set image "$image":$DRONE_SEMVER
+        done
+        IFS="$original_ifs"
+    fi
 else
-    echo "---- A migration job ----"
+    echo ">>> Setting the DRONE_SEMVER <<<"
     DRONE_SEMVER="${DRONE_SEMVER:-latest}"
 fi
 
+echo ">>> Deployment  Manifests: <<<"
 [ -n "${PLUGIN_DEBUG:-false}" ] && kustomize build
+
 
 if [ "$PLUGIN_DRYRUN" = false ]; then
     kustomize build | kubectl apply -f -
